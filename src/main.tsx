b88fa32d465cc58e3a35e5b33dbd2aefaebc6236
Yogi Masaji <yogimasaji02@gmail.com>
2025-03-24 14:42:47 +0700

    small changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Footer from "./Footer.tsx";
+import Footer from "./footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
